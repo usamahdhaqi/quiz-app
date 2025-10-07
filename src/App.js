728ab1375760b2c89085a800ef199de66da31a36
usamahdhaqi <usamahdhaqi@gmail.com>
2025-10-07 20:39:56 +0700

    update loading screen

--- src/App.js
+++ src/App.js
@@ -36,13 +36,13 @@ function App() {
   return (
     <div className="app">
       {/* SVG Icon untuk Judul */}
-      <div className="title-icon">
+      <div className="app-title">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="#8a4fff"
-          width="40px"
-          height="40px"
+          width="38px"
+          height="38px"
         >
           <path d="M12 2C8.13 2 5 5.13 5 9c0 3.28 2.27 6.03 5.33 6.82V18h3.34v-2.18C16.73 15.03 19 12.28 19 9c0-3.87-3.13-7-7-7zm0 11.5c-2.48 0-4.5-2.02-4.5-4.5S9.52 4.5 12 4.5 16.5 6.52 16.5 9 14.48 13.5 12 13.5z" />
         </svg>
@@ -50,7 +50,10 @@ function App() {
       </div>
 
       {loading ? (
-        <p>Loading questions...</p>
+        <div className="loading-container">
+          <div className="spinner"></div>
+          <p>Loading questions...</p>
+        </div>
       ) : (
         <>
           <Quiz questions={questions} />
